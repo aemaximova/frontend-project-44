8a1e5f731b66aa8f121a1b1ba0e381d2a8ba6165
Aitalina <ms.aitam@mail.ru>
2023-09-04 12:31:47 +0600

    bug fix

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = (getQuestion, getCorrectAnswer, name) => {
       console.log('Correct!');
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
-      console.log(`Let's try again, ${name}`);
+      console.log(`Let's try again, ${name}!`);
       win = false;
       break;
     }
